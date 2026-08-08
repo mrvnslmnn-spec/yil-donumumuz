bceeb7e5338491b86c44e6142ddaf15276fdc9b3
mrvnslmnn-spec <mrvnslmnn@gmail.com>
2026-08-08 04:42:00 +0300

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,19 @@
+const photos = [
+    {
+        image: "",
+        title: "İlk anımız",
+        text: "Her şeyin başladığı o güzel gün..."
+    },
+
+    {
+        image: "",
+        title: "Birlikte",
+        text: "Seninle geçen güzel günlerden biri."
+    },
+
+    {
+        image: "",
+        title: "Güzel bir anı",
+        text: "Bu anıyı her hatırladığımda yüzüm gülüyor."
+    }
+];
